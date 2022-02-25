a499c7b3528fe961681094d317f4a7e0d6e9cf13
Hriday Baghar <hbaghar@uw.edu>
2022-02-24 16:39:19 -0800

    Using streamlit's UploadedFileRec class

--- src/backend/data_manipulation.py
+++ src/backend/data_manipulation.py
@@ -4,15 +4,14 @@ import streamlit as st
 class DataFrameHandler(object):
 
     def __init__(self, file):
+        print("Init of df-handler has executed")
         self.df = self.get_dataframe(file)
     
     def get_dataframe(self, file):
         # TO-DO:
         # - Write handler for different file types and return dataframe
-        if file:
-            if file.type == 'csv':
-                return pd.read_csv(file)
-        pass
+        if file.type == 'text/csv':
+            return pd.read_csv(file)
 
     def get_numeric_columns(self):
         pass
@@ -37,24 +36,15 @@ class DataFrameHandler(object):
     
 
 if __name__ == "__main__":
-    from typing import NamedTuple 
-    
-    class UploadedFileRec(NamedTuple):
-        """Metadata and raw bytes for an uploaded file. Immutable."""
-        id: int
-        name: str
-        type: str
-        data: bytes
     ufr=None    
-    filepath = '/Users/ernestocediel/OneDrive - Universidad de los Andes/MSDS/DATA 557 Applied Statistics/Week8-Lin Regression/Sales.csv'
+    filepath = 'src/backend/Sales.csv'
     with open(filepath, 'rb') as fh:
         
         buf = BytesIO(fh.read())
-        ufr=UploadedFileRec(1,'Hello','csv',buf.getvalue())
+        ufr=st.uploaded_file_manager.UploadedFileRec(1,'Hello','text/csv',buf.getvalue())
     
     uf = st.uploaded_file_manager.UploadedFile(ufr)
     obj = DataFrameHandler(uf)
-    print(obj.df)
 
 
 #uploaded_file = st.file_uploader("Choose a CSV file")
