2ee95c97dc2a2f0680004acd40a0907c36b69f2a
Hriday Baghar <hbaghar@uw.edu>
2022-02-27 16:50:42 -0800

    Modified data viz ui and added slider for hist

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from backend import data_manipulation as dm
+from backend import data_viz as dv
 
 @st.cache(allow_output_mutation=True)
 def upload_file(file):
@@ -33,43 +34,49 @@ def data_options(dh):
         return data_option
 
 
-def dataviz(dh):
+def dataviz(dh, viz_type):
     
+    keys = ['viz_type', 'x', 'y', 'bins', 'hue', 'func']
+    inputs = dict.fromkeys(keys)
+
     dataVizOptions = st.container()
     
-    viz_type = x = y = hue = func = None
     with dataVizOptions:
-        viz_type = st.selectbox("What type of visualization do you want?",
-                    ('Histogram', 'Scatterplot', 'Bar Graph', 'Line Graph', 'Box Plot', 'Correlation Matrix'))
+        
+        inputs['viz_type'] = viz_type
+
         if viz_type == "Histogram" or viz_type == "Box Plot":
             if dh.get_categorical_columns():
-                col1, col2 = st.columns(2)
-                x = col1.selectbox("Select a column", dh.get_numeric_columns())
-                hue = col2.selectbox("Select a column to color by", [None] + dh.get_categorical_columns())
+                inputs['x'] = st.selectbox("Select a column", dh.get_numeric_columns())
+                inputs['hue'] = st.selectbox("Select a column to color by", [None] + dh.get_categorical_columns())
             else:
-                x = st.selectbox("Select a column", dh.get_numeric_columns())
+                inputs['x'] = st.selectbox("Select a column", dh.get_numeric_columns())
+            
+            if viz_type == "Histogram":
+                inputs['bins'] = st.slider("Number of bins", min_value=1, max_value=100, value=10)
+        
         elif viz_type == "Scatterplot" or viz_type == "Line Graph":
             if dh.get_categorical_columns():
-                col1, col2, col3 = st.columns(3)
-                x = col1.selectbox("Select X-axis", dh.get_numeric_columns())
-                y = col2.selectbox("Select Y-axis", dh.get_numeric_columns())
-                hue = col3.selectbox("Select a column to color by", [None] + dh.get_categorical_columns())
+                inputs['x'] = st.selectbox("Select X-axis", dh.get_numeric_columns())
+                inputs['y'] = st.selectbox("Select Y-axis", dh.get_numeric_columns())
+                inputs['hue'] = st.selectbox("Select a column to color by", [None] + dh.get_categorical_columns())
             else:
-                col1, col2 = st.columns(2)
-                x = col1.selectbox("Select X-axis", dh.get_numeric_columns())
-                y = col2.selectbox("Select Y-axis", dh.get_numeric_columns())
+                inputs['x'] = st.selectbox("Select X-axis", dh.get_numeric_columns())
+                inputs['y'] = st.selectbox("Select Y-axis", dh.get_numeric_columns())
+        
         elif viz_type == "Bar Graph":
-                if dh.get_categorical_columns():
-                    col1, col2 = st.columns(2)
-                    x = col1.selectbox("Select X-axis", dh.get_categorical_columns())
-                    func = col1.selectbox("Select a function", ('count', 'sum', 'mean', 'median'))
-                    y = col2.selectbox("Select Y-axis", dh.get_numeric_columns())
-                    hue = col2.selectbox("Select a column to color by", [None] + dh.get_categorical_columns())
-                else:
-                    st.write("No categorical columns found in dataset")
+            if dh.get_categorical_columns():
+                inputs['x'] = st.selectbox("Select X-axis", dh.get_categorical_columns())
+                inputs['y'] = st.selectbox("Select Y-axis", dh.get_numeric_columns())
+                inputs['func'] = st.selectbox("Select a function", ('count', 'sum', 'mean', 'median'))
+                inputs['hue'] = st.selectbox("Select a column to color by", [None] + dh.get_categorical_columns())
+            else:
+                st.write("No categorical columns found in dataset")
+        
         else:
             pass
-    return {"viz_type": viz_type, "x": x, "y": y, "hue": hue, "func": func}
+    
+    return inputs
 
 
 def datastat(dh):
@@ -88,8 +95,15 @@ if __name__ == '__main__':
         data_snapshot(filehandler)
         option = data_options(filehandler)
         if option == "Data Visualization":
-            inputs = dataviz(filehandler)
-            st.write(inputs)
+            viz_type = st.selectbox("What type of visualization do you want?",
+                    ('Histogram', 'Scatterplot', 'Bar Graph', 'Line Graph', 'Box Plot', 'Correlation Matrix'))
+            col1, col2 = st.columns([1,2])
+            with col1:
+                inputs = dataviz(filehandler, viz_type)
+                st.write(inputs)
+            with col2:
+                vh = dv.VizHandler(filehandler.df, **inputs)
+                st.pyplot(vh.fig, vh.plot())
         elif  option == "Statistics":
             inputs = datastat(filehandler)
             st.write(inputs)
