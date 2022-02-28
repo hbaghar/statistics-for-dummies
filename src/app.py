e7adf066690139b8724c2778ab4f38336d292522
Hriday Baghar <hbaghar@uw.edu>
2022-02-27 19:41:54 -0800

    Some cleanup and exception handling

--- src/app.py
+++ src/app.py
@@ -1,6 +1,4 @@
-from turtle import onclick
 import streamlit as st
-import pandas as pd
 from backend import data_manipulation as dm
 from backend import data_viz as dv
 
@@ -11,46 +9,52 @@ def upload_file(file):
 def data_snapshot(dh):
     dataSnapshot = st.container()
 
-    with dataSnapshot:
-        #Need to fix ugly table width: plotly table is a viable option
-        st.write("Data Snapshot")
-        st.dataframe(dh.df.head(5))
+    try:
+        with dataSnapshot:
+            #Need to fix ugly table width: plotly table is a viable option
+            st.write("Data Snapshot")
+            st.dataframe(dh.df.head(5))
 
-        st.write("Descriptive Statistics")
-        st.dataframe(dh.df.describe())
+            st.write("Descriptive Statistics")
+            st.dataframe(dh.df.describe())
 
-        col1, col2 = st.columns(2)
-        col2.write("Categorical Statistics")
-        col2.table(dh.df.describe(include=['object']))
+            col1, col2 = st.columns(2)
+            col2.write("Categorical Statistics")
+            col2.table(dh.df.describe(include=['object']))
 
-        col1.write("Missing Value Statistics")
-        col1.dataframe(dh.df.isna().sum())
+            col1.write("Missing Value Statistics")
+            col1.dataframe(dh.df.isna().sum())
+    except:
+        pass
 
 def data_viz_shell(dh):
     viz_type = st.selectbox("What type of visualization do you want?",
                     ('Histogram', 'Scatterplot', 'Bar Graph', 'Line Graph', 'Box Plot', 'Correlation Heatmap'))
-    if viz_type != "Correlation Heatmap":
-        col1, col2 = st.columns([2, 5])
-        with col1:
-            inputs = dataviz_inputs(filehandler, viz_type)
-        with col2:
-            vh = dv.VizHandler(filehandler.df, **inputs)
+    try:
+        if viz_type != "Correlation Heatmap":
+            col1, col2 = st.columns([2, 5])
+            with col1:
+                inputs = dataviz_inputs(filehandler, viz_type)
+            with col2:
+                vh = dv.VizHandler(filehandler.df, **inputs)
+                st.plotly_chart(vh.plot())
+        else:
+            vh = dv.VizHandler(filehandler.df, viz_type=viz_type)
             st.plotly_chart(vh.plot())
-    else:
-        vh = dv.VizHandler(filehandler.df, viz_type=viz_type)
-        st.plotly_chart(vh.plot())
+    except:
+        st.write("Data cannot be visualized")
 
 
 def dataviz_inputs(dh, viz_type):
     
-    keys = ['viz_type', 'x', 'y', 'bins', 'hue', 'func', 'opacity']
-    inputs = dict.fromkeys(keys)
+    inputs = dict()
 
     dataVizOptions = st.container()
     
     with dataVizOptions:
         
         inputs['viz_type'] = viz_type
+        inputs['hue'] = None
 
         if viz_type == "Histogram" or viz_type == "Box Plot":
             if dh.get_categorical_columns():
@@ -61,16 +65,19 @@ def dataviz_inputs(dh, viz_type):
             
             if viz_type == "Histogram":
                 inputs['bins'] = st.slider("Number of bins", min_value=1, max_value=100, value=10)
+            else:    
+                inputs['log_x'] = st.checkbox("Logarithmic x-axis")
         
         elif viz_type == "Scatterplot" or viz_type == "Line Graph":
             if dh.get_categorical_columns():
                 inputs['x'] = st.selectbox("Select X-axis", dh.get_numeric_columns())
                 inputs['y'] = st.selectbox("Select Y-axis", dh.get_numeric_columns())
-                inputs['hue'] = st.selectbox("Select a column to color by", [None] + dh.get_categorical_columns())
-                inputs['opacity'] = st.slider("Opacity", min_value=0.0, max_value=1.0, value=0.7)
+                inputs['hue'] += st.selectbox("Select a column to color by", [None] + dh.get_categorical_columns())
             else:
                 inputs['x'] = st.selectbox("Select X-axis", dh.get_numeric_columns())
                 inputs['y'] = st.selectbox("Select Y-axis", dh.get_numeric_columns())
+            if viz_type == "Scatterplot":
+                inputs['opacity'] = st.slider("Opacity", min_value=0.0, max_value=1.0, value=0.7)
         
         elif viz_type == "Bar Graph":
             if dh.get_categorical_columns():
