edf1f9d7620485c1f791d6a5ddf33eafee7aff83
Hriday Baghar <hbaghar@uw.edu>
2022-03-07 21:58:22 -0800

    Changed input names for z and t test

--- src/backend/hypothesis_test_handler.py
+++ src/backend/hypothesis_test_handler.py
@@ -19,13 +19,13 @@ class TTest(HypothesisTests):
     def perform_test(self):
         if self.num_samples == "One sample":
             x = self.data_handler.df[self.numeric_col]
-            self.results = ht.t_test_1_samp(x, self.mu, self.sig)
+            self.results = ht.t_test_1_samp(x, self.mu, self.significance_level)
         else:
             slice_dict = self.data_handler.slice_by_column(self.cat, self.numeric_col, cat1 = self.cat1, cat2 = self.cat2)
             if self.equal_var:
-                self.results = ht.t_test_welch(slice_dict[self.cat1], slice_dict[self.cat2], self.sig)
+                self.results = ht.t_test_welch(slice_dict[self.cat1], slice_dict[self.cat2], self.significance_level)
             else:
-                self.results = ht.t_test_2_samp_equal_var(slice_dict[self.cat1], slice_dict[self.cat2], self.sig)
+                self.results = ht.t_test_2_samp_equal_var(slice_dict[self.cat1], slice_dict[self.cat2], self.significance_level)
         
             self.results['NaN_found'] = slice_dict['NaN_found']
         return self.results
@@ -39,10 +39,10 @@ class ZTest(HypothesisTests):
     def perform_test(self):
         if self.num_samples == "One sample":
             x = self.data_handler.df[self.numeric_col]
-            self.results = ht.z_test_1_samp(x, self.mu, self.sig)
+            self.results = ht.z_test_1_samp(x, self.mu, self.significance_level)
         else:
             slice_dict = self.data_handler.slice_by_column(self.cat, self.numeric_col, cat1 = self.cat1, cat2 = self.cat2)
-            self.results = ht.z_test_2_samp(slice_dict[self.cat1], slice_dict[self.cat2], self.sig)
+            self.results = ht.z_test_2_samp(slice_dict[self.cat1], slice_dict[self.cat2], self.significance_level)
         
             self.results['NaN_found'] = slice_dict['NaN_found']
         return self.results
@@ -50,13 +50,13 @@ class ZTest(HypothesisTests):
 class ANOVA(HypothesisTests):
 
     def __init__(self, **kwargs):
-        self.numeric_column = kwargs.pop('numeric_column', None)
+        self.numeric_col = kwargs.pop('numeric_col', None)
         self.categorical_column = kwargs.pop('categorical_column', None)
         super().__init__(**kwargs)
 
     def perform_test(self):
-        slice_dict = self.data_handler.slice_by_column(self.categorical_column, self.numeric_column)
-        self.results = ht.anova(slice_dict)
+        slice_dict = self.data_handler.slice_by_column(self.cat, self.numeric_col)
+        # self.results = ht.one_way_anova()
         self.results['NaN_found'] = slice_dict['NaN_found']
         
         return self.results
